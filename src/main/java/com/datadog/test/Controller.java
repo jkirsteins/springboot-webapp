63fb718646e416cc17d5254ef760e63e189f14fc
Jānis Kiršteins <janis.kirsteins@datadoghq.com>
2021-09-27 17:48:36 +0200

    Adding appsec.event tags (tmp)

--- src/main/java/com/datadog/test/Controller.java
+++ src/main/java/com/datadog/test/Controller.java
@@ -1,14 +1,20 @@
 package com.datadog.test;
+import io.opentracing.Span;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentracing.util.GlobalTracer;
 
 @RestController
 public class Controller {
 
     @RequestMapping(value = "/**")
     public String homePage(@RequestBody(required = false) String req) {
+        final Span span = GlobalTracer.get().activeSpan();
+        if (span != null) {
+            span.setTag("appsec.event", true);
+        }
         return "hello world " + req;
     }
 }
