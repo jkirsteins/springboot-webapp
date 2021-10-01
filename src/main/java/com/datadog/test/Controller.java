f5eab29aded32bd511564cc02ddd810a579da31a
Jānis Kiršteins <janis.kirsteins@datadoghq.com>
2021-10-01 17:06:43 +0200

    Adding a way to trigger postgresql spans

--- src/main/java/com/datadog/test/Controller.java
+++ src/main/java/com/datadog/test/Controller.java
@@ -1,6 +1,12 @@
 package com.datadog.test;
 import io.opentracing.Span;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +21,44 @@ public class Controller {
         if (span != null) {
             span.setTag("appsec.event", true);
         }
-        return "hello world " + req;
+
+        // NOTE: see README.md for setting up the docker image to quickly test this
+
+        String url = "jdbc:postgresql://localhost/sportsdb?user=postgres&password=postgres";
+        try (Connection pgConn = DriverManager.getConnection(url)) {
+
+            Statement st = pgConn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM display_names LIMIT 10");
+            while (rs.next())
+            {
+                System.out.print("Column 2 returned ");
+                System.out.println(rs.getString(2));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+            return "pgsql exception :(";
+        }
+
+        // NOTE: SQLite spans do not seem to be sent in the trace for some reason
+
+        // try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:")) {
+        //     try (Statement st = conn.createStatement()) {
+        //         st.execute("restore from /tmp/chinook.db");
+
+        //         // let's see if we can read one of the tables
+        //         ResultSet rs = st.executeQuery("SELECT * FROM artists");
+        //         while (rs.next()) {
+        //             System.out.println(rs.getString(1));
+        //         }
+        //     }
+        // } catch (SQLException e) {
+        //     e.printStackTrace(System.err);
+        //     return "sql exception :(";
+        // }
+
+
+        return "hello in-memory sqlite " + req;
     }
 }
